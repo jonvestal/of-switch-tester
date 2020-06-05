ccfb55d33162d4f7d37ecfe33aab8312bb945af9
Jon Vestal <jonv@vestal-family.org>
2020-06-05 21:35:45 +1000

    Initial commit

--- docker/ryu/files/run_ryu.py
+++ docker/ryu/files/run_ryu.py
@@ -8,7 +8,7 @@ from ryu.cmd import manager
 def main():
     sys.argv.append('--ofp-tcp-listen-port')
     sys.argv.append('6653')
-    sys.argv.append('PipelineTester.py')
+    sys.argv.append('RyuToOpentsdb')
     sys.argv.append('--verbose')
     sys.argv.append('--enable-debugger')
     manager.main()
